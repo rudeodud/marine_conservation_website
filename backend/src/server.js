c1df346bb668ab9f10113b55c8f58d2dd281981b
Gyeongdaeyoung <86515840+rudeodud@users.noreply.github.com>
2026-06-23 10:18:53 +0900

    Docker

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Server is running!");
+    res.json({
+        message: "Marine Backend Running",
+    });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
